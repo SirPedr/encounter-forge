64f4ff3e34adf04d257f07ee3bedff27c44e723d
SirPedr <ppagbpedro@gmail.com>
2025-03-25 02:01:07 -0300

    refactor: preventing name change to update state too frequently

--- src/modules/encounter/components/PartyMember/index.tsx
+++ src/modules/encounter/components/PartyMember/index.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectItem,
@@ -10,8 +9,9 @@ import {
 import { TableCell } from "@/components/ui/table";
 import { type PartyMember as PartyMemberData } from "@/modules/party/types";
 import { SelectContent } from "@radix-ui/react-select";
+import { useDebounce } from "@uidotdev/usehooks";
 import { Trash2 } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const MAX_LEVEL = 20;
 
@@ -20,40 +20,53 @@ type Props = PartyMemberData & {
   onUpdate: (pm: PartyMemberData) => void;
 };
 
-export const PartyMember = ({ id, name, level, onRemove, onUpdate }: Props) => (
-  <React.Fragment>
-    <TableCell>
-      <Input
-        aria-label="Name"
-        value={name}
-        onChange={({ target }) => {
-          onUpdate({ id, level, name: target.value });
-        }}
-      />
-    </TableCell>
+export const PartyMember = ({ id, name, level, onRemove, onUpdate }: Props) => {
+  const [playerName, setPlayerName] = useState(name);
+  const delayedPlayerName = useDebounce(playerName, 300);
 
-    <TableCell>
-      <Select
-        defaultValue={level.toString()}
-        onValueChange={(value) => onUpdate({ id, name, level: Number(value) })}
-      >
-        <SelectTrigger className="w-full" aria-label="Level">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          {Array.from({ length: MAX_LEVEL }, (_, index) => (
-            <SelectItem key={index} value={(index + 1).toString()}>
-              {index + 1}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-    </TableCell>
+  useEffect(() => {
+    if (delayedPlayerName !== name) {
+      onUpdate({ id, level, name: delayedPlayerName });
+    }
+  }, [delayedPlayerName]);
 
-    <TableCell>
-      <Button onClick={() => onRemove(id)} aria-label={`Remove ${name}`}>
-        <Trash2 />
-      </Button>
-    </TableCell>
-  </React.Fragment>
-);
+  return (
+    <React.Fragment>
+      <TableCell>
+        <Input
+          aria-label="Name"
+          value={playerName}
+          onChange={({ target }) => {
+            setPlayerName(target.value);
+          }}
+        />
+      </TableCell>
+
+      <TableCell>
+        <Select
+          defaultValue={level.toString()}
+          onValueChange={(value) =>
+            onUpdate({ id, name, level: Number(value) })
+          }
+        >
+          <SelectTrigger className="w-full" aria-label="Level">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            {Array.from({ length: MAX_LEVEL }, (_, index) => (
+              <SelectItem key={index} value={(index + 1).toString()}>
+                {index + 1}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </TableCell>
+
+      <TableCell>
+        <Button onClick={() => onRemove(id)} aria-label={`Remove ${name}`}>
+          <Trash2 />
+        </Button>
+      </TableCell>
+    </React.Fragment>
+  );
+};
